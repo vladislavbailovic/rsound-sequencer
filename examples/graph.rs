7aed23f6dff9d1662d5f1abcf14c73f104a336ea
vladislav <ve@incsub.com>
2022-07-09 19:26:15 +0200

    Refactor to something more reasonable

--- examples/graph.rs
+++ examples/graph.rs
@@ -2,44 +2,7 @@
 use sequencer::graph::Roll;
 
 use note::*;
-
-struct Sequence {
-    seq: Vec<Note>,
-}
-
-use std::ops::Deref;
-impl Deref for Sequence {
-    type Target = Vec<Note>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.seq
-    }
-}
-
-impl Sequence {
-    pub fn new(seq: Vec<Note>) -> Self {
-        Sequence { seq }
-    }
-
-    pub fn humanize(&mut self) -> &mut Self {
-        let mut result = Vec::new();
-        for &x in &self.seq {
-            if let Note::Tone(p, o, val) = x {
-                let offset = Value::from(1, 128, None);
-                if offset.per_beat() > val.per_beat() {
-                    result.push(Note::Rest(offset));
-                    result.push(Note::Tone(p, o, val - offset));
-                } else {
-                    result.push(x);
-                }
-            } else {
-                result.push(x);
-            }
-        }
-        self.seq = result;
-        self
-    }
-}
+use sequencer::*;
 
 fn get_blocks() -> Vec<(Option<i32>, f32)> {
     Sequence::new(vec![
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,42 @@
 #[cfg(feature = "graph")]
 pub mod graph;
+
+use note::*;
+
+pub struct Sequence {
+    seq: Vec<Note>,
+}
+
+use std::ops::Deref;
+impl Deref for Sequence {
+    type Target = Vec<Note>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.seq
+    }
+}
+
+impl Sequence {
+    pub fn new(seq: Vec<Note>) -> Self {
+        Sequence { seq }
+    }
+
+    pub fn humanize(&mut self) -> &mut Self {
+        let mut result = Vec::new();
+        for &x in &self.seq {
+            if let Note::Tone(p, o, val) = x {
+                let offset = Value::from(1, 128, None);
+                if offset.per_beat() > val.per_beat() {
+                    result.push(Note::Rest(offset));
+                    result.push(Note::Tone(p, o, val - offset));
+                } else {
+                    result.push(x);
+                }
+            } else {
+                result.push(x);
+            }
+        }
+        self.seq = result;
+        self
+    }
+}
