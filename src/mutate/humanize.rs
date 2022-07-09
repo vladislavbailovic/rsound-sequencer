b434a19e4a3d69a51c7e60274b1a33481ddcab78
vladislav <ve@incsub.com>
2022-07-09 21:49:32 +0200

    Implement human mutation 🤘

--- src/mutate/humanize.rs
+++ src/mutate/humanize.rs
@@ -45,8 +45,8 @@ pub struct Humanize {
 impl Default for Humanize {
     fn default() -> Self {
         Self {
-            resolution: 128,
-            range: Range { start: 1, end: 128 },
+            resolution: 256,
+            range: Range { start: 1, end: 32 },
             frequency: 0.5,
         }
     }
@@ -68,7 +68,12 @@ impl Mutator for Humanize {
                 continue;
             }
             if let Note::Tone(p, o, val) = x {
-                let offset = Value::from(1, 128, None);
+                let offset = Value::from(
+                    rng.random(self.range.start, self.range.end),
+                    self.resolution,
+                    None,
+                );
+                eprintln!("humanizing with offset: {:#?}", offset);
                 if offset.per_beat() > val.per_beat() {
                     result.push(Note::Rest(offset));
                     result.push(Note::Tone(p, o, val - offset));
@@ -86,6 +91,7 @@ impl Mutator for Humanize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::Sequence;
 
     #[test]
     fn randomize() {
@@ -101,5 +107,66 @@ mod tests {
 
             prev = rnd;
         }
+    }
+
+    #[test]
+    fn humanize() {
+        let bpm = 60.0;
+        let src = vec![
+            note![A: C0, 1 / 4],
+            pause![1 / 14],
+            note![C: C1, 1 / 4 T],
+            pause![1 / 14],
+            note![A: C0, 1 / 8],
+            pause![1 / 14],
+            note![B: C0, 1 / 8 T],
+        ];
+        let seq = Sequence::new(src.clone());
+        let mut humanized = Sequence::new(src.clone());
+        humanized.transform(Humanize::default());
+
+        let total = seq.iter().map(|x| x.secs(bpm)).sum::<f32>();
+        let human_total = humanized.iter().map(|x| x.secs(bpm)).sum::<f32>();
+        assert_eq!(total, human_total);
+
+        let notes = seq
+            .iter()
+            .map(|x| if x.midi().is_some() { 1 } else { 0 })
+            .sum::<i32>();
+        let human_notes = humanized
+            .iter()
+            .map(|x| if x.midi().is_some() { 1 } else { 0 })
+            .sum::<i32>();
+        assert_eq!(notes, human_notes, "humanization preserves notes count");
+
+        let playtime = seq
+            .iter()
+            .map(|x| if x.midi().is_some() { x.secs(bpm) } else { 0.0 })
+            .sum::<f32>();
+        let human_playtime = humanized
+            .iter()
+            .map(|x| if x.midi().is_some() { x.secs(bpm) } else { 0.0 })
+            .sum::<f32>();
+        assert!(
+            playtime > human_playtime,
+            "expected humanizing to reduce playtime. Original: {}, human: {}",
+            playtime,
+            human_playtime
+        );
+
+        let pausetime = seq
+            .iter()
+            .map(|x| if x.midi().is_none() { x.secs(bpm) } else { 0.0 })
+            .sum::<f32>();
+        let human_pausetime = humanized
+            .iter()
+            .map(|x| if x.midi().is_none() { x.secs(bpm) } else { 0.0 })
+            .sum::<f32>();
+        assert!(
+            pausetime < human_pausetime,
+            "expected humanizing to increase pausetime. Original: {}, human: {}",
+            pausetime,
+            human_pausetime
+        );
     }
 }
